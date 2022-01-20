06ce3cce8dd00145c2997111a29184f2c8150cd6
Mike Burr <mb@unintuitive.org>
2022-01-20 11:03:25 -0700

    generalize (some)

--- curve.py
+++ curve.py
@@ -7,7 +7,6 @@ from field import FieldElement, PrimeGaloisField
 class EllipticCurve:
     a: int
     b: int
-
     field: PrimeGaloisField
 
     def __contains__(self, point: "Point") -> bool:
@@ -22,17 +21,3 @@ class EllipticCurve:
         # Check for membership of curve parameters in the field.
         if self.a not in self.field or self.b not in self.field:
             raise ValueError
-
-
-# Ref: https://en.bitcoin.it/wiki/Secp256k1
-# secp256k1 elliptic curve equation: y² = x³ + 7
-
-# Prime of the finite field
-P: int = (0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)
-field = PrimeGaloisField(prime=P)
-
-# Elliptic curve parameters A and B of the curve : y² = x³ Ax + B
-A: int = 0
-B: int = 7
-
-secp256k1 = EllipticCurve(a=A, b=B, field=field)
